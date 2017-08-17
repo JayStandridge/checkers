f539e4cc48b0f1e88daf5a7db571350d6bac44ea
hellacatholic <pijaypi@gmail.com>
2017-08-17 04:00:35 -0500

    added plot support to better show data
    made two variables, eloNow and eloRecord to make record keeping easier
    and keep the eloNow indexes the same as the corisponding strat

--- checkers.m
+++ checkers.m
@@ -2,12 +2,12 @@ load('values.mat');
 %load('strat.mat');
 %% Make sure to set done=0 if youre starting over
 %also run aiGen to generate new data before
-generations=1;%how many gens you want to run right now
-done=0;
-population = 2;
-children = 1;
+generations=80;%how many gens you want to run right now
+done=20;
+population = 15;
+children = 15;
 total_pop = population + children;
-games=2;%number of opponets an AI will face a generation
+games=5;%number of opponets an AI will face a generation
 d = 6; %number of turns to look ahead
 %I update done manually, Im storing the points data and need how many were
 %done previously to keep up with the indexes
@@ -27,11 +27,11 @@ Mutation_Size_init = 0.25;
 kVal=32;
 %%
 record = zeros(population,generations);
-eloRecord(1:total_pop)=1000;
+eloRecord(1:total_pop,1+done:done+generations)=1000;
+eloNow(1:total_pop,1)=1000;
 for i=1:generations
     Mutation_Rate = Mutation_Rate_init-Mutation_Rate_init*i/generations;
     Mutation_Size = Mutation_Rate_init-Mutation_Rate_init*i/generations;
-    eloRecord=zeros(total_pop,1);
     for j =1:children
         tempvar1=1;
         tempvar2=1;
@@ -44,25 +44,26 @@ for i=1:generations
         tempStrat=Mate(Mom, Dad, Bit_Size, Genes_Total, Gene_Range, Array_Size, Mutation_Rate, Mutation_Size);
         strat(population + j,:)=tempStrat;
     end
-    eloRecord(population+1:total_pop)=1000;
+    eloNow(population+1:total_pop)=1000;
     for j=1:total_pop
         count=0;
         for k=1:games
-            tempvar3=ceil(total_pop*rand);
-            while tempvar3 == j
-                tempvar3=ceil(total_pop*rand);
+            tempVar3=ceil(total_pop*rand);
+            while tempVar3 == j
+                tempVar3=ceil(total_pop*rand);
             end
-            aVal1=fight(strat(j,:),strat(tempvar3,:),d);
+            aVal1=fight(strat(j,:),strat(tempVar3,:),d);
             count=count+1;
-            [eloRecord(j),eloRecord(tempVar3)]=elo(eloRecord(j),eloRecord(tempVar3),aVal1,kVal);
+            [eloNow(j),eloNow(tempVar3)]=elo(eloNow(j),eloNow(tempVar3),aVal1,kVal);
             clc
             fprintf('gen %d of %d\ngame %d of %d',i,generations,(j-1)*(games)+count,(games*total_pop));
             
         end
     end
+    eloRecord(:,done+i)=eloNow;
     for j =1:children
-        strat(find(eloRecord==min(eloRecord),1),:)=[];
-        eloRecord(find(eloRecord==min(eloRecord),1),:)=[];
+        strat(find(eloNow==min(eloNow),1),:)=[];
+        eloNow(find(eloNow==min(eloNow),1))=[];
     end
 %     subplot(2,3,i)
 %     hist(points)
@@ -72,6 +73,19 @@ end
 for i=1:population
     disp(eloRecord(i));
 end
-
 save('strat.mat');
-
+subplot(2,2,1)
+plot(mean(eloRecord))
+title('Average Elo vs Generations')
+subplot(2,2,2)
+plot(max(eloRecord))
+title('Max Elo vs Generations')
+subplot(2,2,3)
+hold on
+plot(eloRecord,'.k','MarkerSize',20)
+boxplot(eloRecord)
+title('Box Plot Vs Generations')
+hold off
+subplot(2,2,4)
+plot(std(eloRecord))
+title('Std Vs Generations')
